01147e32dc1f9616c014e7af96b810ec87847c63
Sally Steuterman <sally.steuterman@gmail.com>
2023-04-06 14:30:37 -0500

    finalizing starter code for exercises

--- part1/exercises/src/App.js
+++ part1/exercises/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import MovieList from './components/MovieList.js';
 import ChoresList from './components/ChoresList.js';
+import BookList from './components/BookList.js';
 
 function App() {
   return (
     <div className="App">
-      <MovieList className="movie" />
+      <MovieList />
       <ChoresList />
+      <BookList />
     </div>
   );
 }
--- part1/exercises/src/components/MovieList.js
+++ part1/exercises/src/components/MovieList.js
@@ -1,5 +1,19 @@
+import React from 'react';
 import classes from './MovieList.module.css';
 
-export default function MovieList () {
-   return <p className = {classes.movieText}>No content is here!</p>;
+class MovieList extends React.Component {
+   render() {
+      const movies = ["Pride and Prejudice", "Howl's Moving Castle"];
+      return (
+      <div>
+         <h3 className = {classes.movieHeading}>Movies I Watched This Month</h3>
+         <ol>
+            <li className={classes.movieText}>{movies[0]}</li>
+            <li className={classes.movieText}>{movies[1]}</li>
+         </ol>
+      </div>
+      );
+   }
 }
+
+export default MovieList;
