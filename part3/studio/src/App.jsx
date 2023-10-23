f26d7a583c7bf8c1f0044250e1eecaec1e46a34f
John Woolbright <jwoolbright23@gmail.com>
2023-10-23 14:53:12 -0500

    vite starter code for part-3-studio

---
+++ part3/studio/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './App.css'
+import BoardAssignment from './components/BoardAssignment';
+import StatusChange from './components/StatusChange';
+import RecipeDisplay from './components/Recipe'; 
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <RecipeDisplay />
+        <div className="recipePhotoBlock">
+          <BoardAssignment />
+          <StatusChange />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
