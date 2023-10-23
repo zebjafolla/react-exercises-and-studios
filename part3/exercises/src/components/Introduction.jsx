8f0bec3a7842731732d8a0102d129990eaa0cc32
John Woolbright <jwoolbright23@gmail.com>
2023-10-23 14:24:12 -0500

    updated part3-exercises with vite project

---
+++ part3/exercises/src/components/Introduction.jsx
@@ -0,0 +1,12 @@
+export default function HobbyIntroduction () {
+   return(
+      <div>
+         <h2>3 Fun Facts About Needlepoint</h2>
+         <ol>
+            <li>Needlepoint uses a specialized canvas as it's base.</li>
+            <li>The most basic stitch is called a tent stitch.</li>
+            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+         </ol>
+      </div>
+   );
+}
