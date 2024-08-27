4a7f61ecc9e6ff4babc379263fb633f69e8dbd2f
zebjafolla <113193465+zebjafolla@users.noreply.github.com>
2024-08-27 07:35:04 -0400

    part 1 exercise 1st commit

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books Ive Read";
+   let book1 = "https://upload.wikimedia.org/wikipedia/en/thumb/8/88/TheLeftHandOfDarkness1stEd.jpg/220px-TheLeftHandOfDarkness1stEd.jpg";
+   let book2 = "https://upload.wikimedia.org/wikipedia/en/thumb/f/fc/TheDispossed%281stEdHardcover%29.jpg/220px-TheDispossed%281stEdHardcover%29.jpg";
+   let book3 = "https://upload.wikimedia.org/wikipedia/en/thumb/5/56/TheNameoftheWind_cover.jpg/220px-TheNameoftheWind_cover.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Picture of the book cover of Left Hand of Darkness by Ursula K Leguin" />
+         <img src={book2} alt="Picture of the book cover of The Dispossed by Ursula K Leguin" />
+         <img src={book3} alt="Picture of the book cover of The Name of The Wind by Patrick Rothfuss" />
       </div>      
    );
 }
--- part1/exercises/src/components/MovieList.jsx
+++ part1/exercises/src/components/MovieList.jsx
@@ -3,13 +3,15 @@ import classes from './MovieList.module.css';
 
 class MovieList extends React.Component {
    render() {
-      const movies = ["Pride and Prejudice", "Howl's Moving Castle"];
+      const movies = ["Pride and Prejudice", "Howl's Moving Castle", "Princess Mononoke", "Spirited Away"];
       return (
       <div>
          <h3 className = {classes.movieHeading}>Movies I Watched This Month</h3>
          <ol>
             <li className={classes.movieText}>{movies[0]}</li>
             <li className={classes.movieText}>{movies[1]}</li>
+            <li className={classes.movieText}>{movies[2]}</li>
+            <li className={classes.movieText}>{movies[3]}</li>
          </ol>
       </div>
       );
